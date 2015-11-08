1bf5212e7274623a39855ab084938aec78cdffae
Bobby Joseph <bobbyjoseph@au1.ibm.com>
2015-11-09 01:20:35 +1100

    Menu fixes, adding images for entry points and other defect fixes

--- themes/tiny-framework-downunder/header.php
+++ themes/tiny-framework-downunder/header.php
@@ -56,7 +56,7 @@
 
 	<?php tha_header_before(); // custom action hook ?>
 
-	<header id="masthead" class="site-header" role="banner" aria-label="page banner" itemscope itemtype="http://schema.org/WPHeader">
+	<header id="masthead" class="site-header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
 		<?php tha_header_top(); // custom action hook ?>
 
@@ -67,7 +67,7 @@
 			?></span>			
 			<div class="header-widget-right">
 				<div class="elist-description">Join our e-list&nbsp;</div>
-				<a href="http://visitor.r20.constantcontact.com/d.jsp?llr=67lifhpab&p=oi&m=1115858153373&sit=lxc8balib&f=b119c4d1-f7ba-41c3-a67b-2afc56cca4fd" title="Sign Up Now (opens new window to 3rd party site)" class="btn btn-custom-black" target="_blank" aria-label="Opens New Window">Sign Up Now</a>
+				<a href="http://visitor.r20.constantcontact.com/d.jsp?llr=67lifhpab&p=oi&m=1115858153373&sit=lxc8balib&f=b119c4d1-f7ba-41c3-a67b-2afc56cca4fd" title="Sign Up Now (opens new window to 3rd party site)" class="btn btn-custom-black" target="_blank">Sign Up Now</a>
 			</div>
 			
 			<div id="site-title-wrapper">
